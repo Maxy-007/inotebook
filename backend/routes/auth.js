e1743b65f4afcff62720d89c805851af182a7b04
Dhruv_solanki <dhruv.trionn@gmail.com>
2023-05-31 16:39:52 +0530

    Create Login Endpoint & send auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,7 @@ var jwt = require('jsonwebtoken');
 
 const JWT_SECRET = 'heyiammax';
 
+// Create User Api
 router.post('/createuser', [
     body('name').isLength({ min: 3 }),
     body('email').isEmail(),
@@ -18,7 +19,7 @@ router.post('/createuser', [
 
     try {
 
-const secPassword = bcrypt.hashSync(req.body.password, 10);
+        const secPassword = bcrypt.hashSync(req.body.password, 10);
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
@@ -26,18 +27,56 @@ const secPassword = bcrypt.hashSync(req.body.password, 10);
         })
 
         const data = {
-            user:{
-               id: user.id
+            user: {
+                id: user.id
             }
         }
 
         const authToken = jwt.sign(data, JWT_SECRET);
-        res.json({authToken})
-        
+        res.json({ authToken })
+
     } catch (error) {
         console.log(error.message);
     }
 
+})
+
+//User Login Api
+
+router.post('/login', [
+    body('email').isEmail(),
+    body('password').exists()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Please Try to login with correct credentials." })
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Please Try to login with correct credentials." })
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken })
+
+    } catch (error) {
+        console.log(error.message);
+    }
 })
 
 module.exports = router;
