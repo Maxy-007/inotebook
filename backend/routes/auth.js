05e2b16f642297183d0521b499a8a0f3dbf3054c
Dhruv_solanki <dhruv.trionn@gmail.com>
2023-05-31 12:27:14 +0530

    hashing password & add jwt authorization in api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,21 +2,42 @@ const express = require('express');
 const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'heyiammax';
 
-router.post('/', [
-    body('name').isLength({min: 3}),
+router.post('/createuser', [
+    body('name').isLength({ min: 3 }),
     body('email').isEmail(),
-],(req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array() });
-  }
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.json(user)).catch(err => {console.log(err); res.json({error: "Please Enter a unique value of email"})})
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+
+const secPassword = bcrypt.hashSync(req.body.password, 10);
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secPassword
+        })
+
+        const data = {
+            user:{
+               id: user.id
+            }
+        }
+
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({authToken})
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+
 })
 
 module.exports = router;
